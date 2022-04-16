0f92d7a73d51a766c193f40dce14b99318ade161
KtKeaton <ktr01@protonmail.com>
2022-04-16 21:07:00 +0800

    init Model Recipe

---
+++ db/migrate/20220416130639_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :money
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_16_125933) do
+ActiveRecord::Schema.define(version: 2022_04_16_130639) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.string "money"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
