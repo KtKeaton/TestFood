d540cb256ed6ffac60de3df0ea10bc3dbeec272c
KtKeaton <ktr01@protonmail.com>
2022-04-17 15:52:18 +0800

    fix path and VC New

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,11 +9,19 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new
+    @recipe = Recipe.new(recipe_params)
     
-    
-    
+    if @recipe.save
+      redirect_to recipes_path, notice: "Successful!"
+    else
+      render :new, notice: "FAILED!"
+    end
   end
 
+private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :money)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :recipe
+  resources :recipes
 
   root to: "recipes#index"
 end
