07f9e42bcb27d1eb374b6bf790d9b228d6e5f801
KtKeaton <ktr01@protonmail.com>
2022-04-17 23:43:44 +0800

    Model sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
+require 'digest'
+
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true, length: { minimum: 4 }, presence: true
+
   has_many :reviews
   has_many :recipes, through: :reviews
+
+  before_create :encrypt_password
+
+  private
+  def encrypt_password
+    salted_pw = "eiojre#{self.password}rewropwr"
+    self.password = Digest::SHA1.hexdigest(salted_pw)
+  end
 end
