ed440463065262622b3f4cc039f00a8f3e1a72c3
KtKeaton <ktr01@protonmail.com>
2022-04-17 23:08:45 +0800

    UsersController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,25 @@
 class UserController < ApplicationController
+  
+  def sign_in
+    @user = User.new
+  end
+
+  def sign_up
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to "/"
+    else
+      render :sign_up
+  end
+
+private
+  def user_params
+    params.request(:user).permit(:name, :password, :password_confirmation, :email)
+  end
+
 end
