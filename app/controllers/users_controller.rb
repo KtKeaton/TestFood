2fd2bb23b7941b1286d5419f7951da295e5be2ee
KtKeaton <ktr01@protonmail.com>
2022-04-18 12:00:44 +0800

    fix Model User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.request(:user).permit(:name, :password, :password_confirmation, :email)
+    params.require(:user).permit(:name, :password, :password_confirmation, :email)
   end
 end
 
